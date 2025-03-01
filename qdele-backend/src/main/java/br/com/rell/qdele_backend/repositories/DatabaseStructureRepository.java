4f6920b26df774a009169a661f8989d1205af339
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-03-01 17:06:25 -0300

    feat: created new repository 'DatabaseStructureRepository'

---
+++ qdele-backend/src/main/java/br/com/rell/qdele_backend/repositories/DatabaseStructureRepository.java
@@ -0,0 +1,9 @@
+package br.com.rell.qdele_backend.repositories;
+
+import br.com.rell.qdele_backend.entities.DatabaseStructure;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DatabaseStructureRepository extends JpaRepository<DatabaseStructure, Long> {
+}
