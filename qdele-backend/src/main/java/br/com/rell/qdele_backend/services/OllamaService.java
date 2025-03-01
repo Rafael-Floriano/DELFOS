7f2477751393611ae3c4e78f0408804561157599
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-03-01 16:15:40 -0300

    feat: created Ollama implementation based on the 'IAiService' contract (using Llama 3.2 as fixed model)

---
+++ qdele-backend/src/main/java/br/com/rell/qdele_backend/services/OllamaService.java
@@ -0,0 +1,27 @@
+package br.com.rell.qdele_backend.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OllamaService implements IAiService {
+
+    @Autowired
+    private OllamaChatModel chatModel;
+
+    @Override
+    public String sendPrompt(final String prompt) {
+        return chatModel.call(
+                new Prompt(
+                        prompt,
+                        OllamaOptions.builder().model("llama3.2").build()
+                )
+        ).getResult().getOutput().getText();
+    }
+
+}
