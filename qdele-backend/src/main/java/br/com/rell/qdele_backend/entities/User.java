1343b09d6544afa6cdb51f8969abb890a440ab4a
corsalover <lucasozpre@gmail.com>
2025-03-02 15:12:53 -0300

    implemented lombok on user.java

--- qdele-backend/src/main/java/br/com/rell/qdele_backend/entities/User.java
+++ qdele-backend/src/main/java/br/com/rell/qdele_backend/entities/User.java
@@ -1,6 +1,7 @@
 package br.com.rell.qdele_backend.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "qdele_user")
 @SQLRestriction("deleted = false")
+@Data
 public class User {
 
     @Id
@@ -24,81 +26,10 @@ public class User {
     private LocalDateTime modifiedDate;
     private Boolean deleted = false;
 
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public Boolean getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    public LocalDateTime getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(LocalDateTime modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
-
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
     public enum Role {
-        SUPER, READER;
+        SUPER, READER
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public User() {
     }
