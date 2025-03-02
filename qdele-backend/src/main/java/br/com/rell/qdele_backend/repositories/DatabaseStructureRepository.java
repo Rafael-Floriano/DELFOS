b7648cc4b0fa8f90e3d862d6f2313b2f242a8b12
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-03-02 05:25:19 -0300

    feat: implemented new query 'findByDatabaseConnectionId' at 'DatabaseStructureRepository'

--- qdele-backend/src/main/java/br/com/rell/qdele_backend/repositories/DatabaseStructureRepository.java
+++ qdele-backend/src/main/java/br/com/rell/qdele_backend/repositories/DatabaseStructureRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DatabaseStructureRepository extends JpaRepository<DatabaseStructure, Long> {
+
+    DatabaseStructure findByDatabaseConnectionId(final Long databaseConnectionId);
+
 }
