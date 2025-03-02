b285252152da75843d969b7adcda0e5ec37d6012
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-03-02 05:26:56 -0300

    feat: implemented new services ('databaseConnectionService', 'databaseStructureService') at service 'PromptService'

--- qdele-backend/src/main/java/br/com/rell/qdele_backend/services/PromptService.java
+++ qdele-backend/src/main/java/br/com/rell/qdele_backend/services/PromptService.java
@@ -1,6 +1,6 @@
 package br.com.rell.qdele_backend.services;
 
-import br.com.rell.qdele_backend.repositories.PromptTemplateRepository;
+import br.com.rell.qdele_backend.entities.DatabaseConnection;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,39 +15,25 @@ public class PromptService {
     private IAiService ollamaService;
     @Autowired
     private PromptTemplateService promptTemplateService;
+    @Autowired
+    private DatabaseConnectionService databaseConnectionService;
+    @Autowired
+    private DatabaseStructureService databaseStructureService;
 
-    private final String mockDatabase = "postgresSQL";
-    private final String mockedDatabaseVersion = "15";
-    private final String mockedDatabaseTables = """
-            table_name	column_name	data_type	character_maximum_length
-            cliente	id	integer	
-            cliente	nome	character varying	100
-            cliente	email	character varying	100
-            cliente	telefone	character varying	20
-            endereco	id	integer	
-            endereco	cliente_id	integer	
-            endereco	rua	character varying	150
-            endereco	numero	character varying	10
-            endereco	complemento	character varying	50
-            endereco	bairro	character varying	100
-            endereco	cidade	character varying	100
-            endereco	estado	character varying	50
-            endereco	pais	character varying	50
-            endereco	cep	character varying	20
-            """;
+    public String process(final Long databaseConnectionId, final String userRequest) {
 
+        final DatabaseConnection databaseConnection = databaseConnectionService.findDatabaseConnection(databaseConnectionId);
+        final String structure = databaseStructureService.findStructureByDatabaseConnectionId(databaseConnectionId);
 
-    public String process(final String userRequest) {
         return ollamaService.sendPrompt(
-                mountPrompt(userRequest)
+                mountPrompt(databaseConnection, structure, userRequest)
         );
     }
 
-    public String mountPrompt(final String userRequest) {
+    public String mountPrompt(final DatabaseConnection databaseConnection, final String structure, final String userRequest) {
         String template = promptTemplateService.findDefaultPromptTemplate();
-        template = template.replace("{database}", mockDatabase);
-        template = template.replace("{database_version}", mockedDatabaseVersion);
-        template = template.replace("{database_tables}", mockedDatabaseTables);
+        template = template.replace("{database}", databaseConnection.getDatabaseType().toString());
+        template = template.replace("{database_tables}", structure);
         template = template.replace("{user_request}", userRequest);
         return template;
     }
