8ad4519fead55d8c2f7f1b3e4640891a1c7c6a8c
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-03-02 21:55:37 -0300

    refactored: removed @Autowired at 'DatabaseConnectionService' and added @RequiredArgsConstructor

--- qdele-backend/src/main/java/br/com/rell/qdele_backend/services/DatabaseConnectionService.java
+++ qdele-backend/src/main/java/br/com/rell/qdele_backend/services/DatabaseConnectionService.java
@@ -3,18 +3,17 @@ package br.com.rell.qdele_backend.services;
 import br.com.rell.qdele_backend.entities.DatabaseConnection;
 import br.com.rell.qdele_backend.repositories.DatabaseConnectionRepository;
 import br.com.rell.qdele_backend.repositories.DatabaseStructureRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class DatabaseConnectionService {
 
-    @Autowired
-    private DatabaseConnectionRepository databaseConnectionRepository;
-    @Autowired
-    private DatabaseStructureRepository databaseStructureRepository;
+    private final DatabaseConnectionRepository databaseConnectionRepository;
+    private final DatabaseStructureRepository databaseStructureRepository;
 
     public DatabaseConnection findDatabaseConnection(final Long databaseConnectionId) {
         return databaseConnectionRepository.findById(databaseConnectionId).orElseThrow(() -> new RuntimeException("Database connection not found: " + databaseConnectionId));
