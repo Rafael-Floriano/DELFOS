5fe2b91ccb00daeac2469ee8f6e5192a3e392ee2
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-04-06 19:04:02 -0300

    feat: created mapper to convert entity to dtos and dtos to entity

--- qdele-backend/src/main/java/br/com/rell/qdele_backend/mapper/DatabaseConnectionMapper.java
+++ qdele-backend/src/main/java/br/com/rell/qdele_backend/mapper/DatabaseConnectionMapper.java
@@ -1,4 +1,25 @@
 package br.com.rell.qdele_backend.mapper;
 
+import br.com.rell.qdele_backend.dto.DatabaseConnectionRequest;
+import br.com.rell.qdele_backend.entities.DatabaseConnection;
+
+import java.time.LocalDateTime;
+
 public class DatabaseConnectionMapper {
+
+    public static DatabaseConnection toEntity(DatabaseConnectionRequest request) {
+        DatabaseConnection entity = new DatabaseConnection();
+        entity.setName(request.name());
+        entity.setUrl(request.host());
+        entity.setPort(request.port());
+        entity.setUsername(request.username());
+        entity.setPassword(request.password());
+        entity.setDatabaseType(request.type());
+
+        entity.setCreatedDate(LocalDateTime.now());
+        entity.setDeleted(false);
+
+        return entity;
+    }
+
 }
