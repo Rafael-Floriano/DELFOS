82a2c8afd150a3ba0e1aff37ebe2159bc646d4fa
Rafael-Floriano <rafael.florianoc9@gmail.com>
2025-04-27 16:36:24 -0300

    refactor: remove DatabaseStructure and PromptTemplate entities, along with their repositories and services

--- qdele-backend/src/main/java/br/com/rell/qdele_backend/repositories/PromptTemplateRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.rell.qdele_backend.repositories;
-
-import br.com.rell.qdele_backend.entities.PromptTemplate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PromptTemplateRepository extends JpaRepository<PromptTemplate, Long> {
-}
